b57bf63553d10b1d150046de19895f68a0a2ff91
mega5800 <43865655+mega5800@users.noreply.github.com>
2020-06-01 19:12:59 +0300

    first try with git: message in Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ import View.*;
 public class Main {
 
     public static void main(String[] args) {
+        //hi from sharon
         LoginFrame mainView = new LoginFrame();
         Model model = new Model();
         Controller controller = new Controller(mainView,model);
